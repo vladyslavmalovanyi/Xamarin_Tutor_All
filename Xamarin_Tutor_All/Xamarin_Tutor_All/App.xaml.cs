d2af10c9c82b91922a897b6b53ffa79d8ee9ba53
Vladyslav Malovanyi <vladyslavmalovanyi@gmail.com>
2018-05-07 00:29:23 +0300

    AbsoluteLayout Task complete Two files added     AbsolutePage_1.xaml     AbsolutePage_1.xaml.cs One file cganged     QuotesPage.xaml.cs

--- Xamarin_Tutor_All/Xamarin_Tutor_All/App.xaml.cs
+++ Xamarin_Tutor_All/Xamarin_Tutor_All/App.xaml.cs
@@ -13,7 +13,7 @@ namespace Xamarin_Tutor_All
 		{
 			InitializeComponent();
 
-			MainPage = new Xamarin_Tutor_All.GridPage_2();
+			MainPage = new Xamarin_Tutor_All.StackPage();
 		}
 
 		protected override void OnStart ()
--- Xamarin_Tutor_All/Xamarin_Tutor_All/QuotesPage.xaml.cs
+++ Xamarin_Tutor_All/Xamarin_Tutor_All/QuotesPage.xaml.cs
@@ -12,9 +12,27 @@ namespace Xamarin_Tutor_All
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class QuotesPage : ContentPage
 	{
-		public QuotesPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+        private static int pos = 0;
+        private static string[] phrases = {
+            "Few are those who see with their own eyes and feel with their own hearts.",
+            "Imagination is more important than knowledge. Knowledge is limited. Imagination encircles the world.",
+            "Unthinking respect for authority is the greatest enemy of truth.",
+            "Try not to become a man of success, but rather try to become a man of value.",
+            "In the middle of difficulty lies opportunity."
+        };
+
+        public QuotesPage()
+        {
+            InitializeComponent();
+            label.Text = phrases[pos];
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (pos < phrases.Length - 1) { pos++; }
+            else { pos = 0; }
+
+            label.Text = phrases[pos];
+        }
+    }
 }
