5b8db1026e195b84e4905dddd9400cbecba655c0
Vladyslav Malovanyi <vladyslavmalovanyi@gmail.com>
2018-05-07 19:07:19 +0300

    Image Exercise complited Button icons for Android/Windows added Two files added     GalleryPage.xaml     GalleryPage.xaml.cs

--- Xamarin_Tutor_All/Xamarin_Tutor_All/App.xaml.cs
+++ Xamarin_Tutor_All/Xamarin_Tutor_All/App.xaml.cs
@@ -13,7 +13,7 @@ namespace Xamarin_Tutor_All
 		{
 			InitializeComponent();
 
-			MainPage = new Xamarin_Tutor_All.GridPage_1();
+			MainPage = new Xamarin_Tutor_All.GalleryPage();
 		}
 
 		protected override void OnStart ()
---
+++ Xamarin_Tutor_All/Xamarin_Tutor_All/GalleryPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Xamarin_Tutor_All
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GalleryPage : ContentPage
+    {
+        private int imageId = 1;
+
+        public GalleryPage()
+        {
+            InitializeComponent();
+
+            imageId = 1;
+
+            NewImage();
+        }
+
+        void NewImage()
+        {
+            mainPicture.Source = new UriImageSource
+            {
+                Uri = new Uri(String.Format("http://lorempixel.com/1920/1080/city/{0}", imageId)),
+                CachingEnabled = false
+            };
+        }
+
+        private void Left_Clicked(object sender, EventArgs e)
+        {
+            imageId--;
+            if (imageId < 1) { imageId = 10; }
+
+            NewImage();
+        }
+
+        private void Right_Clicked(object sender, EventArgs e)
+        {
+            imageId++;
+            if (imageId > 10) { imageId = 1; }
+
+            NewImage();
+        }
+    }
+}
